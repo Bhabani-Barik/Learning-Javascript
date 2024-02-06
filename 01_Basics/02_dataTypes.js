866efb581f75257055d6a4fd0670b171fb041a41
Bhabani Shankar Barik <114484023+Bhabani-Barik@users.noreply.github.com>
2024-02-06 06:21:58 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version.
+
+// alert( 3 + 3) // we are using nodejs, not browser.
+
+console.log( 3 
+    +
+     3) // We can't write code like this.Code readability should be high.
+
+console.log("Bhabani")
+
+let name = "Bhabani"
+let age = 18
+let isLoggedIn = false
+
+/*
+Primitive DataTypes :
+
+1. number => range : 2 to power 53
+2. bigint => Specially used in tradding , stockMarket , bigWebsite like reddit, faceBook etc.
+3. string => " " 
+4. boolean => true/false
+5. null => standalone  value (representation of empty value) and null is 
+    a object.
+6. undefined => value is not asssigned to the variable.but its declared
+7. symbol => unique
+
+
+1. Objects
+
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
