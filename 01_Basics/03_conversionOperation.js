afdc601e34b2c5e12d86b3b94f8ad52b3417e3da
Bhabani Shankar Barik <114484023+Bhabani-Barik@users.noreply.github.com>
2024-02-07 02:53:50 +0000

    Conversion of dataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+// let score = 33;
+// let score = "33";//33
+// let score = "33abc";//NaN
+// let score = null;//0
+// let score = undefined;//NaN
+// let score = true;//1
+// let score = false;//0
+// let score = "Bhabani"; //NaN
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+let isLoggedIn = "Bhabani";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+/* 
+Note: 
+1 => true; 0 => false 
+
+"" => false ; "Bhabani" => true
+*/
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
