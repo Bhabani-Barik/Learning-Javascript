4c2358dfbbac02dd590994d4625027285e6f755a
Bhabani Shankar Barik <114484023+Bhabani-Barik@users.noreply.github.com>
2024-02-08 02:57:05 +0000

    Js comparision done

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+/* Avoid this type of comparision in most cases .Focus more clean code and clean code have more value.
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+*/
+
+// ===
+
+console.log("2" == 2); //true
+console.log("2" === 2); // false
