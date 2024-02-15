ce1b977f1329f5f1c1dbea75d60c8e7cac62e897
Bhabani Shankar Barik <114484023+Bhabani-Barik@users.noreply.github.com>
2024-02-15 15:26:00 +0000

    scopes in Js

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -36,7 +36,52 @@ let a = 300;
 if(true) {
     let a = 10;
     const b = 20;
-    console.log("INNER: ", a); // 10
+    // console.log("INNER: ", a); // 10
 }
 
-console.log(a); // 300
+// console.log(a); // 300
+
+/******************** Nested scope ******************/ 
+
+function one(){
+    const username = "bhabani";
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    
+    // console.log(website); // error 'child property can't access by parent'
+
+    two(); //bhabani 'parent property can access by child'
+}
+
+// one();
+
+
+if(true) {
+    const username = "bhabani";
+    if(username === "bhabani") {
+        const website = " youtube";
+        // console.log(username + website);// bhabani youtube
+    }
+    //console.log(website);//error
+}
+// console.log(username);
+
+
+/************************** Interesting Topic ************************/
+
+// console.log(addone(5));
+
+function addone(num){
+    return num + 1;
+}
+
+// addone(5); 
+
+const addTwo = function(num){
+    return num + 2;
+}
+
+addTwo(5);
