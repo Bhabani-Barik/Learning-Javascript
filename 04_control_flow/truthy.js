e013c9c4da677165ce4ddf1d8a3c5e528b8f443c
Bhabani Shankar Barik <114484023+Bhabani-Barik@users.noreply.github.com>
2024-02-19 17:12:40 +0000

    Control Flow in JS

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,69 @@
+// const userEmail = "xyz@user.ai";
+const userEmail = [];
+
+if ( userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+/** 
+Falsy Values:
+    -> false
+    -> 0
+    -> -0
+    -> BigInt 0n
+    -> ""
+    -> null
+    -> undefined
+    -> NaN
+
+Truthy Values
+    -> "0"
+    -> 'false' / "false"
+    -> " "
+    -> []
+    -> {}
+    -> function(){}
+    -> 
+
+ */
+//Array check
+if(userEmail.length === 0) {
+    console.log("Array is empty");
+}
+
+//Object check
+
+const emptyObj = {};
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("Array is empty");
+}
+
+/** 
+ Note: 
+ =====
+    -> false == 0 // true
+    -> false == '' //true
+    -> 0 == '' // true
+ */
+
+/**  Nullish Coalescing Operator (??) : null, undefined */
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 15;
+
+val1 = null ?? 10 ?? 20; 
+
+console.log(val1);
+
+
+/** Terniary Operator */
+
+// condition ? true : false;
+
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("More than 80");
