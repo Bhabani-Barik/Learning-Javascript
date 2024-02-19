0be969a927fa3b6cf2c822a42702b487ddf9bfcd
Bhabani Shankar Barik <114484023+Bhabani-Barik@users.noreply.github.com>
2024-02-19 12:31:24 +0000

    control flow in Js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,51 @@
+/** if statement  :
+
+ In javascript 'if statement' will execute when condition is true. If condition is false then 'if statement' will not be execute. 
+
+if (true){
+    // Statement will be execute.
+}
+
+if(false){
+    // Statement will not be execute.
+}
+
+*/
+
+/**  Comparison Operator 
+ <, >, <=, >=, ==, !=, ===, !==
+
+const temperature = 31;
+
+if(temperature === 42 ){
+    console.log("less than 40");
+} else {
+    console.log("temperature is greater than 40");
+}
+
+console.log("executed");  */
+
+
+// Scope
+// const score = 200;
+
+// if(score > 100) {
+//     const power = "fly";
+//     console.log(`User power:${power}`);
+// }
+// console.log(`User power:${power}`); // error: power is not defined (block scope)
+
+
+/** Short hand notation 
+
+const balance = 1000;
+
+// if (balance > 500) console.log("test");// implicit scope 
+// if(balance > 500) console.log("test"), console.log("test2");//not recommend 
+
+*/
+
+/** Nesting  */
+
+const balance = 1000;
+
