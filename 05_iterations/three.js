d790c53f8d9b977b2060b34763c80f3501b0b6ee
Bhabani Shankar Barik <114484023+Bhabani-Barik@users.noreply.github.com>
2024-02-20 04:22:42 +0000

    for of loop in Js

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,4 +1,11 @@
-/** for of loop */
+/************** for of loop ***************
+
+Syntax: 
+   for (const iterator of object) {
+   
+   }
+
+ */
 
 /** 
  -> In Js array and object is most powerful.
@@ -27,7 +34,7 @@
     // console.log(`Each char is ${greet}`);
  }
 
- /************************ Maps **************************************
+ /************************ Map **************************************
   
 -> The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value.
 -> Map objects are collections of key-value pairs. A key in the Map may only occur once; it is unique in the Map's collection.
@@ -65,3 +72,9 @@
 //  for (const [key, value] of myObject) {
 //     console.log(`${key} :- ${value}`);// error : myObject is not iterable
 //  }
+
+/**
+ Object is not iterable in for-of loop
+ */
+
+
