3f0bd792ca930eef6d63c7e7344c056a8d7f9de3
Bhabani Barik <bhabanishankar.barik21@gmail.com>
2024-03-09 15:31:44 +0530

    Promises  in  Javascript

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -7,63 +7,95 @@
 -> https://github.com/v8
 */
 
-
 // Promise Creation
-const promiseOne = new Promise(function(resolve, reject){
-    // Do an async task
-    // DB calls, cryptography, network realated
-    setTimeout(function(){
-        console.log('Async task is completed.');
-        resolve();
-    } , 1000);
+const promiseOne = new Promise(function (resolve, reject) {
+  // Do an async task
+  // DB calls, cryptography, network realated
+  setTimeout(function () {
+    console.log("Async task is completed.");
+    resolve();
+  }, 1000);
 });
 
 // Promise Consumption
-promiseOne.then(function(){
-    console.log("Promise Consumed");
+promiseOne.then(function () {
+  console.log("Promise Consumed");
 });
 
 // New Promise Creation
-new Promise(function(resolve, reject){
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve();
-    }, 1000);
-}).then(function(){ 
-    console.log("Async 2 resolved");
+new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function () {
+  console.log("Async 2 resolved");
 });
 
 // third promise creation
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({username: "green-eye", email: "eye@example.com"});
-    }, 1000);
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve({ username: "green-eye", email: "eye@example.com" });
+  }, 1000);
 });
 
-promiseThree.then(function(user){
-    console.log(user);
+promiseThree.then(function (user) {
+  console.log(user);
 });
 
 //Promise four
-const promiseFour = new Promise(function(resolve, reject){
-    setTimeout(function(){
-      let error = false;
-      if ( !error ){
-        resolve({username:"bhabani",password: "123"});
-      } else {
-        reject('ERROR: Something went wrong')
-      }  
-    },1000);
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = false;
+    if (!error) {
+      resolve({ username: "bhabani", password: "123" });
+    } else {
+      reject("ERROR: Something went wrong");
+    }
+  }, 1000);
 });
 
- promiseFour.then((user) => {
+promiseFour
+  .then((user) => {
     console.log(user);
     return user.username;
-}).then((username) => {
-  console.log(username);
-}).catch(function(error){
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch(function (error) {
     console.log(error);
-}).finally(() => console.log("The promise is either resolved or rejected."));
-
+  })
+  .finally(() => console.log("The promise is either resolved or rejected."));
 
 // Promise five
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    // let error = false;
+    if (!error) {
+      resolve({ username: "javascript", password: "123" });
+    } else {
+      reject("ERROR: JS went wrong");
+    }
+  }, 2000);
+});
+
+// async function consumePromiseFive(){
+//     const response = await promiseFive;
+//     console.log(response);
+// }
+
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+consumePromiseFive();
+
+
+
