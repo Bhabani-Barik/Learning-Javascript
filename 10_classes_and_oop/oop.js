9882c9caed500f493e6780bb72527ce58999c86f
Bhabani Barik <bhabanishankar.barik21@gmail.com>
2024-03-10 10:16:14 +0530

    Constructor in js

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -19,3 +19,29 @@ const user = {
 // console.log(this);
 
 
+//Constructor function 
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    // return this;
+    // return keyword Implicitely return the value 
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.user}`);
+    }
+
+    return this;
+}
+
+// const userOne = User("bhabani", 21, true);
+// const userTwo = User("Javascript", 12, true);
+
+
+const userOne = new User("bhabani", 21, true);
+const userTwo = new User("Javascript", 12, true);
+// console.log(userOne);
+// console.log(userTwo);
+console.log(userOne.constructor);
